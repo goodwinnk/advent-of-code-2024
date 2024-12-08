fad75b402d9bb2a5948507489a9386f7e60efd09
Nikolay Krasko <goodwinnk@gmail.com>
2024-12-08 14:16:43 +0100

    Day 6, clean all fancy logic, simplify, still wrong

--- src/bin/06.rs
+++ src/bin/06.rs
@@ -3,10 +3,8 @@ use std::hash::Hash;
 use advent_of_code2024_rust::{day, run_on_day_input};
 use anyhow::*;
 use std::io::{BufRead};
-use std::result;
 use itertools::Itertools;
 use linked_hash_set::LinkedHashSet;
-use crate::Action::{Move, TurnRight};
 use crate::Cell::{Empty, Wall};
 
 #[derive(Hash, Eq, PartialEq, Copy, Clone, Debug)]
@@ -24,15 +22,6 @@ impl Direction {
             Direction::RIGHT => Direction::DOWN,
             Direction::DOWN => Direction::LEFT,
             Direction::LEFT => Direction::UP,
-        }
-    }
-
-    fn turn_left(&self) -> Direction {
-        match self {
-            Direction::UP => Direction::LEFT,
-            Direction::RIGHT => Direction::UP,
-            Direction::DOWN => Direction::RIGHT,
-            Direction::LEFT => Direction::DOWN,
         }
     }
 }
@@ -69,12 +58,6 @@ impl Map {
     }
 }
 
-#[derive(Hash, Eq, PartialEq)]
-enum Action {
-    Move,
-    TurnRight,
-}
-
 fn apply_shift(coordinate: &Coordinate, map: &Map, shift: (i32, i32)) -> Option<Coordinate> {
     let (dx, dy) = shift;
     let x = coordinate.x as i32 + dx;
@@ -105,15 +88,6 @@ fn down(coordinate: &Coordinate, map: &Map) -> Option<Coordinate> {
     apply_shift(coordinate, map, (0, 1))
 }
 
-fn left_hand_coordinate(position: &Position, map: &Map) -> Option<Coordinate> {
-    match position.direction {
-        Direction::UP => left(&position.coordinate, map),
-        Direction::RIGHT => up(&position.coordinate, map),
-        Direction::DOWN => right(&position.coordinate, map),
-        Direction::LEFT => down(&position.coordinate, map)
-    }
-}
-
 fn step_forward_coordinate(position: &Position, map: &Map) -> Option<Coordinate> {
     match position.direction {
         Direction::UP => up(&position.coordinate, map),
@@ -123,43 +97,24 @@ fn step_forward_coordinate(position: &Position, map: &Map) -> Option<Coordinate>
     }
 }
 
-fn move_guard(position: &Position, map: &Map) -> (Action, Option<Position>) {
-    match step_forward_coordinate(position, map) {
-        None => (Move, None),
-        Some(next_coordinate) => {
-            if map.map[next_coordinate.y][next_coordinate.x] == Wall {
-                return (TurnRight, Some(Position {
-                    direction: position.direction.turn_right(),
-                    coordinate: position.coordinate,
-                }));
-            }
+fn move_guard(position: &Position, map: &Map, additional_wall: Option<Coordinate>) -> Option<Position> {
+    let next_coordinate = step_forward_coordinate(position, map)?;
 
-            (Move, Some(Position {
-                direction: position.direction.clone(),
-                coordinate: next_coordinate,
-            }))
-        }
-    }
-}
-
-fn backward_empty_cell(position: &Position, map: &Map) -> Option<Position> {
-    let backward_coordinate = match position.direction {
-        Direction::UP => down(&position.coordinate, map),
-        Direction::RIGHT => left(&position.coordinate, map),
-        Direction::DOWN => up(&position.coordinate, map),
-        Direction::LEFT => right(&position.coordinate, map)
-    }?;
-
-    if map.get_cell(backward_coordinate) != Empty {
-        return None;
+    if map.get_cell(next_coordinate) == Wall ||
+        additional_wall.map_or(false, |coordinate| coordinate == next_coordinate) {
+        return Some(Position {
+            direction: position.direction.turn_right(),
+            coordinate: position.coordinate,
+        });
     }
 
     Some(Position {
-        direction: position.direction,
-        coordinate: backward_coordinate,
+        direction: position.direction.clone(),
+        coordinate: next_coordinate,
     })
 }
 
+#[allow(dead_code)]
 fn visited_map(map: &Map, visited: &LinkedHashSet<Position>) -> String {
     let trace_map: HashMap<Coordinate, Direction> =
         visited.iter().map(|position| (position.coordinate, position.direction)).collect();
@@ -228,115 +183,66 @@ fn read_input<R: BufRead>(reader: R) -> Result<(Coordinate, Map)> {
     Ok((start.unwrap(), map))
 }
 
+fn build_trace(position: &Position, map: &Map, additional_wall: Option<Coordinate>) -> (bool, LinkedHashSet<Position>) {
+    let mut trace: LinkedHashSet<Position> = LinkedHashSet::new();
+    let mut cur_position = position.clone();
+
+    loop {
+        if !trace.insert(cur_position) {
+            return (true, trace);
+        }
+
+        let next_position = move_guard(&cur_position, &map, additional_wall);
+        if next_position.is_none() {
+            return (false, trace);
+        }
+
+        cur_position = next_position.unwrap();
+    }
+}
+
 //noinspection DuplicatedCode
 fn part1<R: BufRead>(reader: R) -> Result<i64> {
     let (start, map) = read_input(reader)?;
-
-    let mut cur_position = Some(Position {
+    let start_position = Position {
         direction: Direction::UP,
         coordinate: start,
-    });
+    };
 
-    let mut trace: HashSet<Position> = HashSet::new();
-    trace.insert(cur_position.unwrap());
-
-    while cur_position.is_some() {
-        let (_, next_position) = move_guard(cur_position.as_ref().unwrap(), &map);
-        if next_position.is_some() {
-            if !trace.insert(next_position.unwrap()) {
-                break;
-            }
-        }
-        cur_position = next_position;
-    }
+    let (_, trace) = build_trace(&start_position, &map, None);
 
     let visited_coordinates = trace.iter()
         .map(|position: &Position| position.coordinate)
         .collect::<HashSet<Coordinate>>();
 
-    Ok(visited_coordinates
-        .len() as i64)
-}
-
-fn build_back_on_track(
-    position: &Position,
-    map: &Map,
-    back_on_track: &HashSet<Position>,
-) -> LinkedHashSet<Position> {
-    let mut result = LinkedHashSet::new();
-    let mut queue = Vec::new();
-    queue.push(position.clone());
-    while !queue.is_empty() {
-        let next = queue.pop().unwrap();
-        if !result.insert(next) || back_on_track.contains(&next) {
-            continue
-        }
-
-        if let Some(left_hand) = left_hand_coordinate(&next, &map) {
-            if map.get_cell(left_hand) == Wall {
-                queue.push(Position {
-                    direction: next.direction.turn_left(),
-                    coordinate: next.coordinate.clone(),
-                });
-            }
-        }
-        if let Some(backward) = backward_empty_cell(&next, &map) {
-            queue.push(backward);
-        }
-    }
-
-    result
+    Ok(visited_coordinates.len() as i64)
 }
 
 //noinspection DuplicatedCode
 fn part2<R: BufRead>(reader: R) -> Result<i64> {
     let (start, map) = read_input(reader)?;
 
-    let mut cur_position = Position {
+    let starting_position = Position {
         direction: Direction::UP,
         coordinate: start,
     };
 
-    let mut back_on_track: HashSet<Position> = HashSet::new();
-    let mut trace: LinkedHashSet<Position> = LinkedHashSet::new();
-
-    let mut obstacles_coordinates: HashSet<Coordinate> = HashSet::new();
-
-    loop {
-        if !trace.insert(cur_position) {
-            panic!("Loop in the original track. It's not expected.")
-        }
-
-        back_on_track.extend(build_back_on_track(&cur_position, &map, &back_on_track));
-
-        let turn_right_position = Position {
-            direction: cur_position.direction.turn_right(),
-            coordinate: cur_position.coordinate,
-        };
-
-        if back_on_track.contains(&turn_right_position) {
-            if let Some(obstacle_coordinate) = step_forward_coordinate(&cur_position, &map) {
-                if map.get_cell(obstacle_coordinate) == Empty {
-                    if obstacle_coordinate != start {
-                        obstacles_coordinates.insert(obstacle_coordinate);
-                    }
+    let (_, trace) = build_trace(&starting_position, &map, None);
+    let additional_walls = trace.iter()
+        .map(|position| {
+            step_forward_coordinate(&position, &map).and_then(|obstacle_coordinate| {
+                if map.get_cell(obstacle_coordinate) == Empty && obstacle_coordinate != start {
+                    build_trace(position, &map, Some(obstacle_coordinate)).0.then_some(obstacle_coordinate)
+                } else {
+                    None
                 }
-            }
-        }
+            })
+        })
+        .filter(|opt| opt.is_some())
+        .map(|opt| opt.unwrap())
+        .collect::<HashSet<Coordinate>>();
 
-        let (_, next_position_opt) = move_guard(&cur_position, &map);
-        if next_position_opt.is_none() {
-            break;
-        }
-
-        cur_position = next_position_opt.unwrap();
-    }
-
-    println!(
-        "{}",
-        visited_map(&map, &trace));
-
-    Ok(obstacles_coordinates.iter().count() as i64)
+    Ok(additional_walls.iter().count() as i64)
 }
 
 //#region
@@ -360,20 +266,23 @@ fn main() {
 
 //noinspection SpellCheckingInspection
 #[cfg(test)]
-mod part1_tests {
+mod tests {
+    use super::*;
     use std::io::BufReader;
     use indoc::indoc;
-    use super::*;
 
-    fn test_part1(expect: i64, input: &str) {
-        assert_eq!(expect, part1(BufReader::new(input.as_bytes())).unwrap());
-    }
+    mod part1_tests {
+        use super::*;
 
-    #[test]
-    fn test1() {
-        test_part1(
-            41,
-            indoc! {"
+        fn test_part1(expect: i64, input: &str) {
+            assert_eq!(expect, part1(BufReader::new(input.as_bytes())).unwrap());
+        }
+
+        #[test]
+        fn test1() {
+            test_part1(
+                41,
+                indoc! {"
                 ....#.....
                 .........#
                 ..........
@@ -385,31 +294,29 @@ mod part1_tests {
                 #.........
                 ......#...
             "},
-        );
+            );
+        }
+
+        #[test]
+        fn part1_final() {
+            assert_eq!(5239i64, run_on_day_input(day!(), part1).unwrap());
+        }
     }
 
-    #[test]
-    fn part1_final() {
-        part1_result().unwrap();
-    }
-}
+    //noinspection SpellCheckingInspection
+    #[cfg(test)]
+    mod part2_tests {
+        use super::*;
 
-//noinspection SpellCheckingInspection
-#[cfg(test)]
-mod part2_tests {
-    use std::io::BufReader;
-    use indoc::indoc;
-    use super::*;
+        fn test_part2(expect: i64, input: &str) {
+            assert_eq!(expect, part2(BufReader::new(input.as_bytes())).unwrap());
+        }
 
-    fn test_part2(expect: i64, input: &str) {
-        assert_eq!(expect, part2(BufReader::new(input.as_bytes())).unwrap());
-    }
-
-    #[test]
-    fn test1() {
-        test_part2(
-            6,
-            indoc! {"
+        #[test]
+        fn test1() {
+            test_part2(
+                6,
+                indoc! {"
                 ....#.....
                 .........#
                 ..........
@@ -421,63 +328,79 @@ mod part2_tests {
                 #.........
                 ......#...
             "},
-        );
-    }
+            );
+        }
 
-    #[test]
-    fn test2() {
-        test_part2(
-            2,
-            indoc! {"
+        #[test]
+        fn test2() {
+            test_part2(
+                2,
+                indoc! {"
                 .#...
                 ....#
                 #^...
                 ..#..
             "},
-        );
-    }
+            );
+        }
 
-    #[test]
-    fn test3() {
-        test_part2(
-            2,
-            indoc! {"
+        #[test]
+        fn test3() {
+            test_part2(
+                2,
+                indoc! {"
                 .#...
                 ....#
                 #....
                 .^#..
             "},
-        );
-    }
+            );
+        }
 
-    #[test]
-    fn test4() {
-        test_part2(
-            1,
-            indoc! {"
+        #[test]
+        fn test4() {
+            test_part2(
+                1,
+                indoc! {"
                 ....
                 ...#
                 #...
                 .^#.
             "},
-        );
-    }
+            );
+        }
 
-    #[test]
-    fn test5() {
-        test_part2(
-            1,
-            indoc! {"
+        #[test]
+        fn test5() {
+            test_part2(
+                1,
+                indoc! {"
                 ....
                 .^.#
                 #...
                 ..#.
             "},
-        );
-    }
+            );
+        }
 
-    #[test]
-    fn part2_final() {
-        part2_result().unwrap();
+        #[test]
+        fn test6() {
+            test_part2(
+                1,
+                indoc! {"
+                ...#...
+                ......#
+                ..#....
+                #^...#.
+                ..#....
+            "},
+            );
+        }
+
+        #[test]
+        fn part2_final() {
+            assert_eq!(1819i64, run_on_day_input(day!(), part2).unwrap());
+        }
     }
 }
+
