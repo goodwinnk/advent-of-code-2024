1660cff9aa606ba7178606113be44003123df43c
Nikolay Krasko <goodwinnk@gmail.com>
2025-01-04 00:33:30 +0100

    Day 16, part 2, AI couldn't solve it

--- src/bin/16.rs
+++ src/bin/16.rs
@@ -1,5 +1,5 @@
 use std::cmp::Ordering;
-use std::collections::{BinaryHeap, HashSet};
+use std::collections::{BinaryHeap, HashMap, HashSet, VecDeque};
 use advent_of_code2024_rust::{day, run_on_day_input};
 use anyhow::*;
 use std::io::{BufRead};
@@ -13,14 +13,20 @@ fn parse_input<R: BufRead>(reader: R) -> Array2D<char> {
         .filter(|line| !line.is_empty())
         .map(|line| line.trim().chars().collect())
         .collect();
-    
+
     Array2D::from_rows(&rows).unwrap()
+}
+
+#[derive(Clone, Copy, Debug, Eq, Hash, PartialEq)]
+struct Node {
+    position: Coordinate,
+    direction: Direction
 }
 
 #[derive(Clone, Copy, Debug, Eq, PartialEq, Hash)]
 struct State {
-    position: Coordinate,
-    direction: Direction,
+    node: Node,
+    previous: Option<Node>,
     cost: u64
 }
 
@@ -84,32 +90,37 @@ fn part1<R: BufRead>(reader: R) -> Result<u64> {
     let mut heap = BinaryHeap::new();
 
     heap.push(State {
-        position: start,
-        direction: Direction::Right,
+        node: Node {
+            position: start,
+            direction: Direction::Right,
+        },
+        previous: None,
         cost: 0
     });
 
     while let Some(current) = heap.pop() {
-        if current.position == end {
+        if current.node.position == end {
             return Ok(current.cost);
         }
 
-        let state_key = (current.position, current.direction);
-        if visited.contains(&state_key) {
+        if visited.contains(&current.node) {
             continue;
         }
-        visited.insert(state_key);
+        visited.insert(current.node);
 
         for &next_direction in &directions {
-            if next_direction.opposite_direction() == current.direction {
+            if next_direction.opposite_direction() == current.node.direction {
                 continue
             }
 
-            if let Some(next_pos) = can_move(&maze, current.position, next_direction) {
-                let turn_cost = if next_direction != current.direction { 1000 } else { 0 };
+            if let Some(next_pos) = can_move(&maze, current.node.position, next_direction) {
+                let turn_cost = if next_direction != current.node.direction { 1000 } else { 0 };
                 let next_state = State {
-                    position: next_pos,
-                    direction: next_direction,
+                    node: Node {
+                        position: next_pos,
+                        direction: next_direction,
+                    },
+                    previous: None,
                     cost: current.cost + 1 + turn_cost
                 };
 
@@ -122,8 +133,101 @@ fn part1<R: BufRead>(reader: R) -> Result<u64> {
 }
 
 //noinspection DuplicatedCode
-fn part2<R: BufRead>(_reader: R) -> Result<i64> {
-    Ok(0)
+fn part2<R: BufRead>(reader: R) -> Result<u64> {
+    let maze = parse_input(reader);
+
+    let start = find_position(&maze, 'S');
+    let end = find_position(&maze, 'E');
+
+    let directions = [
+        Direction::Right,
+        Direction::Down,
+        Direction::Left,
+        Direction::Up
+    ];
+
+    let mut distances: HashMap<Node, (u64, Vec<Node>)> = HashMap::new();
+    let mut best_states = BinaryHeap::new();
+
+    let start_node = Node {
+        position: start,
+        direction: Direction::Right,
+    };
+    best_states.push(State {
+        node: start_node.clone(),
+        previous: None,
+        cost: 0
+    });
+
+    let mut min_distance_to_end = None;
+
+    while let Some(current) = best_states.pop() {
+        if min_distance_to_end.is_some() {
+            if current.cost > min_distance_to_end.unwrap() {
+                // Any other path is worse than this one
+                break;
+            };
+        }
+
+        if let Some((distance, previous)) = distances.get_mut(&current.node) {
+            if current.cost > *distance {
+                continue;
+            } else if current.cost == *distance {
+                previous.push(current.previous.unwrap());
+                continue;
+            } else {
+                panic!("Shouldn't be less because of using BinaryHeap! \
+                    node: {:?}, cost: {}, distance: {}", current.node, current.cost, *distance);
+            }
+        }
+
+        if current.node != start_node {
+            distances.insert(current.node, (current.cost, vec![current.previous.unwrap()]));
+        } else {
+            distances.insert(current.node, (0, vec![]));
+        }
+
+        if current.node.position == end {
+            min_distance_to_end = Some(current.cost);
+            continue;
+        }
+
+        for &next_direction in &directions {
+            if next_direction.opposite_direction() == current.node.direction {
+                continue
+            }
+
+            if let Some(next_pos) = can_move(&maze, current.node.position, next_direction) {
+                let turn_cost = if next_direction != current.node.direction { 1000 } else { 0 };
+                let next_state = State {
+                    node: Node {
+                        position: next_pos,
+                        direction: next_direction,
+                    },
+                    previous: Some(current.node),
+                    cost: current.cost + 1 + turn_cost
+                };
+
+                best_states.push(next_state);
+            }
+        }
+    }
+
+    let mut optimal_path_coordinates: HashSet<Coordinate> = HashSet::default();
+    let mut path_traverse: VecDeque<Node> = VecDeque::default();
+
+    optimal_path_coordinates.insert(end);
+    for direction in directions.iter() {
+        if let Some((_, nodes)) = distances.get(&Node { position: end, direction: *direction }) {
+            path_traverse.extend(nodes.iter());
+        };
+    }
+    while let Some(node) = path_traverse.pop_front() {
+        optimal_path_coordinates.insert(node.position);
+        path_traverse.extend(distances[&node].1.iter());
+    }
+
+    Ok(optimal_path_coordinates.len() as u64)
 }
 
 //#region
@@ -234,23 +338,63 @@ mod tests {
     mod part2_tests {
         use super::*;
 
-        fn test_part2(expect: i64, input: &str) {
+        fn test_part2(expect: u64, input: &str) {
             assert_eq!(expect, part2(BufReader::new(input.as_bytes())).unwrap());
         }
 
         #[test]
         fn test1() {
             test_part2(
-                0,
+                45,
                 indoc! {"
-                1   2
+                ###############
+                #.......#....E#
+                #.#.###.#.###.#
+                #.....#.#...#.#
+                #.###.#####.#.#
+                #.#.#.......#.#
+                #.#.#####.###.#
+                #...........#.#
+                ###.#.#####.#.#
+                #...#.....#.#.#
+                #.#.#.###.#.#.#
+                #.....#...#.#.#
+                #.###.#.#.#.#.#
+                #S..#.....#...#
+                ###############
+            "}
+            );
+        }
+
+        #[test]
+        fn test2() {
+            test_part2(
+                64,
+                indoc! {"
+                #################
+                #...#...#...#..E#
+                #.#.#.#.#.#.#.#.#
+                #.#.#.#...#...#.#
+                #.#.#.#.###.#.#.#
+                #...#.#.#.....#.#
+                #.#.#.#.#.#####.#
+                #.#...#.#.#.....#
+                #.#.#####.#.###.#
+                #.#.#.......#...#
+                #.#.###.#####.###
+                #.#.#...#.....#.#
+                #.#.#.#####.###.#
+                #.#.#.........#.#
+                #.#.#.#########.#
+                #S#.............#
+                #################
             "}
             );
         }
 
         #[test]
         fn part2_final() {
-            part2_result().unwrap();
+            assert_eq!(533, run_on_day_input(day!(), part2).unwrap());
         }
     }
 }
